0778f7d915398b64cd898a13799f3154653f8c63
WiktorOdoc <135710227+WiktorOdoc@users.noreply.github.com>
2025-10-23 21:58:51 +0200

    Change pin

--- scr/test/java/put/io/selenium/atm/AtmCardInfoTest.java
+++ scr/test/java/put/io/selenium/atm/AtmCardInfoTest.java
@@ -29,7 +29,7 @@ public class AtmCardInfoTest {
 	@Test
 	public void testTryPinCorrect() {
 		fixture.cardIn();
-		fixture.tryPin("1525");
+		fixture.tryPin("69420");
 		assertTrue(fixture.pinWasOk);
 	}
 	
